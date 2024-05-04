1c53a0105cc99baf88e1e478e7930dbd66e82aab
mohsinsultan <mohsinsultan14k@gmail.com>
2024-05-04 11:50:19 +0530

    Before copying from SB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
-import { NgxSpinnerService } from "ngx-spinner";
+import { Component } from '@angular/core'; 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular16';
 
-  constructor(private spinner: NgxSpinnerService) { }
+  constructor() { }
+
   marks: any =
-    {
+  {
       colorRemaining: { color: '#373636', border: '#373636' },
       colorProgress: { color: '#373636', border: '#373636' },
       type: 'line',
@@ -20,29 +21,28 @@ export class AppComponent {
       majorSize: '9%',
       majorInterval: 10,
       minorInterval: 2
-    };
+  };
   labels: any =
-    {
-      offset: '88%',
+  {
+      offset: '28%',
       step: 10,
-      visible: true
-    };
+      visible: false
+  };
   progressBar: any =
-    {
-      size: '70%',
+  {
+      size: '90%',
       offset: '0%',
       background: {
-        stroke: '#373636', strokeWidth: 1, fill: { color: '#a7a7a7', gradientType: "linear", gradientStops: [[0, 1], [50, 0.5], [100, 1]] }
+          stroke: '#373636', strokeWidth: 1, fill: { color: 'blue', gradientType: "linear", gradientStops: [[0, 1], [50, 0.5], [100, 1]] }
       }
-    };
+  };
   pointer: any =
-    {
-      type: 'circle', style: { fill: { color: '#a4a3a3', gradientType: "linear", gradientStops: [[0, 0.5], [50, 0.6], [100, 1]] }, stroke: '#333' },
-      size: '10%', offset: '50%'
-    };
+  {
+      type: 'circle', style: { fill: { color: 'black', gradientType: "linear", gradientStops: [[0, 0.5], [50, 0.6], [100, 1]] }, stroke: '#333' },
+      size: '24%', offset: '60%'
+  };
 
   onvolchange(val: any) {
     console.log(val.args.value)
   }
 }
-
